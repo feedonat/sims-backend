959b2b5fd2390a240e5d7b8b643d2a8436c9af18
kedir yibrie <feedonat@gmail.com>
2017-08-24 15:40:56 -0400

    logger added

--- sms-app/src/main/java/com/highcode/sms/controller/HomeController.java
+++ sms-app/src/main/java/com/highcode/sms/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.highcode.sms.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String home() {
+		
+		
+			logger.info("getWelcome is executed!");
+		
+
 		return "index";
 	}
 }
